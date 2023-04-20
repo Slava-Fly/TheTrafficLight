b360661fc4ac3e43313eff16a87cb077ea4b501e
@Slava-Fly <school.94@bk.ru>
2023-04-20 20:54:00 +0800

    Initial Commit

---
+++ TheTrafficLight/TheTrafficLightApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheTrafficLightApp.swift
+//  TheTrafficLight
+//
+//  Created by User on 20.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct TheTrafficLightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
